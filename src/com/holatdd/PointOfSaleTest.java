fd8b245c9d764cff62db6451498c1a13cc73a9e8
Niko <cogani@yahoo.es>
2013-04-08 15:04:19 +0200

    Refactoring: moving common vars to setup method

--- src/com/holatdd/PointOfSaleTest.java
+++ src/com/holatdd/PointOfSaleTest.java
@@ -1,29 +1,36 @@
 package com.holatdd;
+
 import static org.mockito.Mockito.*;
 
 // No es necesario importar Assert, pues no lo usamos directamente
 //import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
-
 
 public class PointOfSaleTest {
 
+	private Catalog catalog;
+	private Screen screen;
+	private PointOfSale pointOfSale;
+
+	@Before
+	public void setup() {
+		catalog = mock(Catalog.class);
+		screen = mock(Screen.class);
+		pointOfSale = new PointOfSale(catalog, screen);
+	}
+
 	@Test
-	public void onBarcode_search_catalog()throws Exception{
-		Catalog catalog = mock(Catalog.class);
-		Screen screen= mock(Screen.class);
-		PointOfSale pointOfSale = new PointOfSale(catalog, screen);
+	public void onBarcode_search_catalog() throws Exception {
+
 		pointOfSale.onBarcode("123");
 		verify(catalog).search("123");
 	}
-	
+
 	@Test
-	public void onBarcode_show_price()throws Exception{
-		Catalog catalog = mock(Catalog.class);
-		Screen screen= mock(Screen.class);
+	public void onBarcode_show_price() throws Exception {
 		when(catalog.search("123")).thenReturn("1€");
-		PointOfSale pointOfSale = new PointOfSale(catalog, screen);
 		pointOfSale.onBarcode("123");
 
 		verify(screen).show("1€");
